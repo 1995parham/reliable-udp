277e4c8f1fae725c3f47119c67f387c7436476f2
Parham Alvani <parham.alvani@gmail.com>
2019-04-11 18:02:45 +0430

    Adds special message types.

--- message.h
+++ message.h
@@ -2,6 +2,7 @@
 #define MESSAGE_H
 
 #define MAX_LINE 80
+#define WINDOW_SIZE 10
 
 #pragma pack(1)
 struct message {
@@ -9,5 +10,9 @@ struct message {
     char payload[MAX_LINE];
 };
 #pragma pack(0)
+
+#define MESSAGE_FIN 0x02
+#define MESSAGE_ACK 0x03
+#define MESSAGE_EMPTY 0x04
 
 #endif
